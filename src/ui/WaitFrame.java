5e0839f9a1e13fa0e1f9cfe5f376940fa4a11a8c
BUAALisa <1658441344@qq.com>
2023-11-28 19:21:42 +0800

    WaitingWindowSize

--- src/ui/WaitFrame.java
+++ src/ui/WaitFrame.java
@@ -11,16 +11,16 @@ public class WaitFrame extends JFrame {
 
 	public WaitFrame() {
 		// 设置名字
-		this.setTitle("迷你大富翁-Java版");
+		this.setTitle("MonopolyGame");
 		// 设置默认关闭属性（程序结束）
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 设置窗口大小
-		this.setSize(380, 370);
-		// 不允许用户改变窗口大小
-		this.setResizable(false);
+		this.setSize(1200, 800);
+		// 允许用户改变窗口大小
+		this.setResizable(true);
 		// 居中
 		FrameUtil.setFrameCenter(this);
-		add(new JLabel("加载中，请稍后...",JLabel.CENTER));
+		add(new JLabel("loading...",JLabel.CENTER));
 		setVisible(true);
 	}
 
